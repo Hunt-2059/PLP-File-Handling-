895cfecaed51bc230d438ea42d60fec982b4859c
Hunt-2059 <strangejason165@gmail.com>
2025-09-30 11:31:18 +0300

    Create input.py

---
+++ input.py
@@ -0,0 +1,18 @@
+# Hunt-2059
+# This script reads from 'input.txt', processes the data, and writes to 'output.txt'.
+import os
+
+print("Looking in:", os.getcwd())  # shows where Python is searching
+
+try:
+    with open("input.txt", "r") as infile:
+        data = infile.readlines()
+        print(data)
+    processed_data = [line.upper() for line in data]
+    print(processed_data)
+    
+    with open("output.txt", "w") as outfile:
+        outfile.writelines(processed_data)
+        print("Write operation successful.")
+except FileNotFoundError:
+    print("File not found. Try checking file path.")
